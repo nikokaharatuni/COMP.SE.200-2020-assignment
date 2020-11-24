1773d101687681adb004ee196ed50400029d76e8
Ville Vaarala <ville.l.vaarala@gmail.com>
2020-11-24 16:20:45 +0200

    Add unit tests for isTypedArray

---
+++ __tests__/isTypedArray.test.js
@@ -0,0 +1,11 @@
+import isTypedArray from "../src/isTypedArray";
+
+describe("5.32 isTypedArray", () => {
+  it("0. should tell an Uint8Array is a typed array", () => {
+    expect(isTypedArray(new Uint8Array())).toBe(true);
+  });
+
+  it("1. should not tell an array is a typed array", () => {
+    expect(isTypedArray([])).toBe(false);
+  });
+});
