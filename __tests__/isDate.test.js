acf6661325546c9ee80430d15f88020c2b2f6ac2
Ville Vaarala <ville.l.vaarala@gmail.com>
2020-11-24 14:53:48 +0200

    Add unit tests for isDate
    Note that non-100% coverage is because of branched implementation for Node and non-Node environments. The tests only cover the non-Node branch.

---
+++ __tests__/isDate.test.js
@@ -0,0 +1,25 @@
+import isDate from "../src/isDate";
+
+describe("5.26 isDate", () => {
+  it("0. should tell a date is a date", () => {
+    expect(isDate(new Date())).toBe(true);
+  });
+
+  it("1. should not tell a string timestamp is a date", () => {
+    expect(isDate("Mon April 23 2012")).toBe(false);
+  });
+
+  it("2. should not tell a number is a date", () => {
+    expect(isDate(1601381729834)).toBe(false);
+  });
+
+  it("3. should not tell an object with getDate method is a date", () => {
+    expect(
+      isDate({
+        getDate() {
+          return 1;
+        }
+      })
+    ).toBe(false);
+  });
+});
